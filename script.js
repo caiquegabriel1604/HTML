2499763fdfa59085856876657c85437066f05a23
caiquegabriel1604 <caique.braga.brito@escola.pr.gov.br>
2025-04-11 09:37:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-function atualizarRelogio() {
-    const relogio = document.getElementById('relogio')
-    //Aqui eu vou mexer no elementp com ID relogio no HTML
-    const agora = new Date();
-    agora.setHours(agora.getHours()-3)
-    //Essa linha serve para definir para fuso horario correto
-    const horas = String(agora.getHours()).padStart(2,'0');
-    const minutos = String(agora.getMinutes()).padStart(2,'0');
-    const segudos = String(agora.getSeconds()).padStart(2,'0');
-    relogioElement.textContent = `${horas}:${minutos}:${segudos}`
-}
+function hora(){
+    //relogio recebe data atual
+    let relogio = document.getElementById('relogio')
+
+    let tempo = new Date();
+    let horas = tempo.getHours();
+    let minutos = tempo.getMinutes();
+    let segundos = tempo.getSeconds();
+
+    relogio.innerHTML =`${horas}:${minutos}:${segundos}`
+   
+
+
+
+
+  }
+  
+  hora();
